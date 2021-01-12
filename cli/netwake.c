d9940a216753a4624598bad070718d09df2cb0ab
Sijmen J. Mulder <ik@sjmulder.nl>
2021-01-12 23:15:07 +0100

    CLI: add woltab support
    The 'Wake-on-LAN table' woltab is a file that specifies names for MAC
    addresses, similar to /etc/hosts, and laid out like so:
    
      01:12:34:56:78:9a my-pc
    
    and search for at:
    
      $XGD_CONFIG_HOME/woltab (usually in ~/.config)
      $HOME/.woltab
      $SYSCONFDIR/woltab (usually in /etc)
    
    The CLI then accepts names in addition to MAC addresses.

--- cli/netwake.c
+++ cli/netwake.c
@@ -1,21 +1,92 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <err.h>
 #include "../common/wol.h"
+#include "config.h"
+
+#ifndef PREFIX
+#define PREFIX "/usr/local"
+#endif
+
+static char *
+lookup_in(const char *path, const char *name)
+{
+	static char l[1024], val[64], valname[64];
+	char *p;
+	FILE *f;
+
+	fprintf(stderr, "trying %s\n", path);
+
+	if (!(f = fopen(path, "r"))) {
+		fprintf(stderr, "%s: noent\n", path);
+		return NULL;
+	}
+
+	while ((p = fgets(l, sizeof(l), f))) {
+		if ((p = strchr(l, '#')))
+			*p = '\0';
+		if (sscanf(l, " %64s %64s", val, valname) != 2)
+			continue;
+		if (!strcmp(valname, name)) {
+			fclose(f);
+			return val;
+		}
+	}
+
+	fclose(f);
+	return NULL;
+}
+
+static char *
+lookup(const char *name)
+{
+	static char p[1024];
+	char *home, *home_cfg, *val;
+
+	home = getenv("HOME");
+	home_cfg = getenv("XDG_CONFIG_HOME");
+
+	if (home_cfg) {
+		snprintf(p, sizeof(p), "%s/woltab", home_cfg);
+		if ((val = lookup_in(p, name)))
+			return val;
+	} else if (home) {
+		snprintf(p, sizeof(p), "%s/.config/woltab", home);
+		if ((val = lookup_in(p, name)))
+			return val;
+	}
+
+	if (home) {
+		snprintf(p, sizeof(p), "%s/.woltab", home);
+		if ((val = lookup_in(p, name)))
+			return val;
+	}
+
+	return lookup_in(SYSCONFDIR "/woltab", name);
+}
 
 int
 main(int argc, char **argv)
 {
 	struct mac_addr mac;
+	const char *s;
 
 	if (argc != 2)
-		fputs("usage: netwake <mac-addr>\n", stderr);
-	else if (mac_parse(argv[1], &mac) == -1)
-		fputs("netwake: invalid MAC address\n", stderr);
-	else if (wol_send(&mac) == -1)
-		fputs("netwake: failed tom send packet\n", stderr);
-	else {
-		puts("WOL packet sent");
-		return 0;
-	}
+		errx(1, "usage: netwake <mac-addr>");
 
-	return 1;
+	if (mac_parse(argv[1], &mac) != -1)
+		;
+	else if (!(s = lookup(argv[1])))
+		errx(1, "invalid MAC address and not in woltab: %s",
+		    argv[1]);
+	else if (mac_parse(s, &mac) == -1)
+		errx(1, "invaid MAC in woltab: %s", s);
+
+	if (wol_send(&mac) == -1)
+		errx(1, "failed to send packet");
+
+	printf("WOL packet sent!\n");
+	return 0;
 }
